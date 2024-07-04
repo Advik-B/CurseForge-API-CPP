b73e8b1d2c73c593a68e144143266a02fb75bbde
Advik <advik.b@gmail.com>
2024-07-04 18:19:15 +0530

    Create CurseForgeEngine.cpp

---
+++ src/CurseForgeEngine.cpp
@@ -0,0 +1,23 @@
+#include "CurseForgeEngine.hpp"
+
+namespace cf {
+
+    CurseForgeAPI::CurseForgeAPI(const string &api_key, bool use_cache) {
+        this->api_key = api_key;
+        this->use_cache = use_cache;
+    }
+
+    CurseForgeAPI::CurseForgeAPI(const string &api_key) {
+        this->api_key = api_key;
+    }
+
+    json CurseForgeAPI::fetch(const string &url) {
+        auto headers = Header{
+                    {"Accept", "application/json"},
+                    {"x-api-key", this->api_key},
+                    {"Content-Type", "application/json"},
+                    };
+        const auto response = Get(Url{CURSEFORGE_API + url}, headers);
+        return json::parse(response.text);
+    }
+}
