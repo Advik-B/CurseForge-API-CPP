67b8d685de0f7ed484b5fad28b4043feec166976
Advik <advik.b@gmail.com>
2024-07-04 22:32:05 +0530

    Update CurseFileIndex.hpp

--- src/CurseFileIndex.hpp
+++ src/CurseFileIndex.hpp
@@ -15,14 +15,13 @@ using std::string;
 using std::optional;
 
 class CurseFileIndex {
+public:
     string gameVersion;
     int32_t fileId;
     string filename;
     CurseFileReleaseType releaseType;
     optional<int32_t> gameVersionTypeId = std::nullopt;
     CurseModLoaderType modLoader;
-
-public:
     static CurseFileIndex from_json(const json &data);
 };
 
