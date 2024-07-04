9f91d6c44b1814d779777e8d5639de1825f0260f
Advik <advik.b@gmail.com>
2024-07-04 19:11:10 +0530

    Update CurseCategory.cpp

--- src/CurseCategory.cpp
+++ src/CurseCategory.cpp
@@ -2,14 +2,34 @@
 #include "CurseCategory.hpp"
 
 namespace cf {
-    CurseCategory::CurseCategory(const json &data) {
-        this->id = data["id"];
-        this->gameId = data["gameId"];
-        this->name = data["name"];
-        this->slug = data["slug"];
-        this->url = data["url"];
-        this->iconUrl = data["iconUrl"];
-        this->dateModified = data["dateModified"];
+
+    CurseCategory CurseCategory::from_json(const json &data) {
+        CurseCategory category;
+        category.id = data["id"];
+        category.gameId = data["gameId"];
+        category.name = data["name"];
+        category.slug = data["slug"];
+        category.url = data["url"];
+        category.iconUrl = data["iconUrl"];
+        category.dateModified = data["dateModified"];
+        if (data.contains("isClass")) {
+            category.isClass = data["isClass"];
+        }
+        if (data.contains("classId")) {
+            category.classId = data["classId"];
+        }
+        if (data.contains("parentCategoryId")) {
+            category.parentCategoryId = data["parentCategoryId"];
+        }
+        if (data.contains("displayIndex")) {
+            category.displayIndex = data["displayIndex"];
+        }
+        return category;
+    }
+
+    CurseCategory CurseCategory::from_id(int32_t id, CurseForgeAPI &api) {
+        auto response = api.fetch("v1/categories/" + std::to_string(id));
+        return from_json(response);
     }
 
     CurseCategory::~CurseCategory() = default;
