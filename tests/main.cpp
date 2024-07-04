7e4fddd7986305a031779aabf10c0ea4d318291c
Advik <advik.b@gmail.com>
2024-07-04 23:33:03 +0530

    Update main.cpp

--- tests/main.cpp
+++ tests/main.cpp
@@ -13,7 +13,6 @@ int main() {
         const auto mod = cf::CurseMod::from_id(1, api);
         cout << mod.name << endl;
     } catch (const cf::errors::CurseAPIError& e) {
-        cout << "Mod not found" << endl;
         cout << e.what() << endl;
     }
     // auto response = api.fetch("/v1/categories");
