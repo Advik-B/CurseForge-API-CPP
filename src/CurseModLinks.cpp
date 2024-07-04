c8fdf36b4eb7e523fc7a5e9323df0820bd72476c
Advik <advik.b@gmail.com>
2024-07-04 23:52:48 +0530

    Update CurseModLinks.cpp

--- src/CurseModLinks.cpp
+++ src/CurseModLinks.cpp
@@ -3,18 +3,18 @@
 #include <iostream>
 namespace cf {
     CurseModLinks CurseModLinks::from_json(const json &data) {
-        // std::cout << data.dump(4) << std::endl;
+        std::cout << data.dump(4) << std::endl;
         CurseModLinks links;
-        if (data.contains("websiteUrl")) {
+        if (data.contains("websiteUrl") && !data["websiteUrl"].empty()) {
             links.websiteUrl = data["websiteUrl"];
         }
-        if (data.contains("wikiUrl")) {
+        if (data.contains("wikiUrl") && !data["wikiUrl"].empty()){
             links.wikiUrl = data["wikiUrl"];
         }
-        if (data.contains("issuesUrl")) {
+        if (data.contains("issuesUrl") && !data["issuesUrl"].empty()) {
             links.issuesUrl = data["issuesUrl"];
         }
-        if (data.contains("sourceUrl")) {
+        if (data.contains("sourceUrl") && !data["sourceUrl"].empty()) {
             links.sourceUrl = data["sourceUrl"];
         }
         return links;
