fa93704ab57bc38f63b3930e84322b9b0f9ea6c1
Advik <advik.b@gmail.com>
2024-07-04 21:45:20 +0530

    Create CurseModLoaderType.hpp

---
+++ src/CurseModLoaderType.hpp
@@ -0,0 +1,19 @@
+
+#ifndef CURSEMODLOADERTYPE_HPP
+#define CURSEMODLOADERTYPE_HPP
+
+namespace cf {
+
+enum class CurseModLoaderType {
+    ANY = 0,
+    FORGE = 1,
+    CAULDRON = 2,
+    LITELOADER = 3,
+    FABRIC = 4,
+    QUILT = 5,
+    NEOFORGE = 6,
+};
+
+} // cf
+
+#endif //CURSEMODLOADERTYPE_HPP
