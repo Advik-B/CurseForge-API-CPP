6715eea6cb236accccefcbaa08d2c27b099628dd
Advik <advik.b@gmail.com>
2024-07-04 22:15:54 +0530

    Update CurseModAsset.cpp

--- src/CurseModAsset.cpp
+++ src/CurseModAsset.cpp
@@ -2,4 +2,14 @@
 #include "CurseModAsset.hpp"
 
 namespace cf {
+    CurseModAsset CurseModAsset::from_json(const json &data) {
+        CurseModAsset asset;
+        asset.id = data["id"];
+        asset.modId = data["modId"];
+        asset.title = data["title"];
+        asset.description = data["description"];
+        asset.thumbnailUrl = data["thumbnailUrl"];
+        asset.url = data["url"];
+        return asset;
+    }
 } // cf
