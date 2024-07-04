102cd751592ee5134f154df2530e5cc118c68641
Advik <advik.b@gmail.com>
2024-07-04 22:59:24 +0530

    Update main.cpp

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,14 +2,21 @@
 #include <string>
 #include <CurseForgeAPI.hpp>
 #include <CurseMod.hpp>
+
+#include "CurseErrors.hpp"
 // #include "CurseMod.hpp"
 using namespace std;
 
 
 int main() {
     cf::CurseForgeAPI api("$2a$10$evexg5YEn66/ch1BwoJItePBJ.zySowOsp74e3vSFsKNNN7tRtH4u");
-    const auto mod = cf::CurseMod::from_id(1, api);
-    cout << mod.name << endl;
+    try {
+        const auto mod = cf::CurseMod::from_id(1, api);
+        cout << mod.name << endl;
+    } catch (const cf::CurseAPIError& e) {
+        cout << "Mod not found" << endl;
+        cout << e.what() << endl;
+    }
     // auto response = api.fetch("/v1/categories");
     // cout << response.dump(4) << endl;
     return 0;
