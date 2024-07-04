867da00a53452e9c9037b62e3fd954b8884aead3
Advik <advik.b@gmail.com>
2024-07-04 18:19:17 +0530

    Create CurseForgeEngine.hpp

---
+++ src/CurseForgeEngine.hpp
@@ -0,0 +1,28 @@
+
+#ifndef CURSEFORGEENGINE_HPP
+#define CURSEFORGEENGINE_HPP
+
+#include <cpr/cpr.h>
+#include <nlohmann/json.hpp>
+#include <string>
+
+namespace cf {
+    using json = nlohmann::json;
+    using namespace cpr;
+    using std::string;
+
+    class CurseForgeAPI {
+    private:
+        const string CURSEFORGE_API = "https://api.curseforge.com/";
+        bool use_cache = false;
+        string api_key;
+
+    public:
+        CurseForgeAPI(const string &api_key, bool use_cache = false);
+        CurseForgeAPI(const string &api_key);
+        ~CurseForgeAPI();
+        json fetch(const string &url);
+    };
+};
+
+#endif //CURSEFORGEENGINE_HPP
