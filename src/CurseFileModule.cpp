9afc473903e17406f7a709117e0a6ac2a3e639a0
Advik <advik.b@gmail.com>
2024-07-04 22:49:54 +0530

    Update CurseFileModule.cpp

--- src/CurseFileModule.cpp
+++ src/CurseFileModule.cpp
@@ -1,1 +1,11 @@
 
+#include "CurseFileModule.hpp"
+
+namespace cf {
+CurseFileModule CurseFileModule::from_json(const json &data) {
+    CurseFileModule module;
+    module.name = data["name"];
+    module.fingerprint = data["fingerprint"];
+    return module;
+}
+}
