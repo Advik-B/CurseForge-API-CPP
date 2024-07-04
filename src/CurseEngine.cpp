a527d354377707b0c6370b944890826c0ad996e7
Advik <advik.b@gmail.com>
2024-07-04 19:19:05 +0530

    Update CurseEngine.cpp

--- src/CurseEngine.cpp
+++ src/CurseEngine.cpp
@@ -1,22 +1,30 @@
 #include "CurseEngine.hpp"
+#include "CurseErrors.hpp"
 
 namespace cf {
     CurseForgeAPI::CurseForgeAPI(const string &api_key) {
         this->api_key = api_key;
     }
 
-    json CurseForgeAPI::fetch(string &path) {
+    json CurseForgeAPI::fetch(const string &path) {
         // If the path starts with a slash, remove it
-        if (path[0] == '/') {
-            path = path.substr(1);
+        string path_ = path;
+        if (path_.front() == '/') {
+            path_ = path_.substr(1);
         }
         auto headers = Header{
                     {"Accept", "application/json"},
                     {"x-api-key", this->api_key},
                     {"Content-Type", "application/json"},
                     };
-        const auto response = Get(Url{CURSEFORGE_API + path}, headers);
-        return json::parse(response.text);
+        const auto response = Get(Url{CURSEFORGE_API + path_}, headers);
+
+        try {
+            return json::parse(response.text);
+        } catch (const std::exception &e) {
+            throw CurseAPIError(e.what());
+        }
+
     }
 
     CurseForgeAPI::~CurseForgeAPI() = default;
