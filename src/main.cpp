103fe922558b4402aa2c22f71aa1d6031dc79436
Advik <advik.b@gmail.com>
2024-07-04 18:37:15 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@ using namespace std;
 
 
 int main() {
-    cf::CurseForgeAPI api("x-api-key");
+    cf::CurseForgeAPI api("$2a$10$evexg5YEn66/ch1BwoJItePBJ.zySowOsp74e3vSFsKNNN7tRtH4u");
+    auto response = api.fetch("v1/games");
+    cout << response.dump(4) << endl;
     return 0;
 }
