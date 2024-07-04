ecb79c030f55542264cc1463da2fb5e417481d74
Advik <advik.b@gmail.com>
2024-07-05 00:35:36 +0530

    Update main.cpp

--- tests/main.cpp
+++ tests/main.cpp
@@ -7,14 +7,14 @@ using namespace std;
 
 
 int main() {
-    cf::CurseForgeAPI api("$2a$10$evexg5YEn66/ch1BwoJItePBJ.zySowOsp74e3vSFsKNNN7tRtH4u");
-    // try {
+    cf::CurseForgeAPI api("$2a$10$evexg5YEn66/ch1BwoJItePBJ.zySowOsp74x3vSFsKNNN7tRtH4u");
+    try {
         const auto mod = cf::CurseMod::from_id(238222, api);
         cout << mod.name << endl;
         cout << mod.authors[0].name << endl;
-    // } catch (const cf::errors::CurseAPIError& e) {
-        // cout << e.what() << endl;
-    // }
+    } catch (const cf::errors::CurseAPIError& e) {
+        cout << e.what() << endl;
+    }
     // auto response = api.fetch("/v1/categories");
     // cout << response.dump(4) << endl;
     return 0;
