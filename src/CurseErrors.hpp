40c8a410644b98cf3ca35128d7ca75ddb9816790
Advik <advik.b@gmail.com>
2024-07-04 23:12:08 +0530

    Update CurseErrors.hpp

--- src/CurseErrors.hpp
+++ src/CurseErrors.hpp
@@ -4,19 +4,28 @@
 
 #include <exception>
 #include <utility>
+#include <string>
 
-namespace cf {
-    class CurseAPIError final : public std::exception {
+namespace cf::errors {
+    using std::string;
+    using std::move;
+    using std::exception;
+
+    class CurseAPIError : public std::exception {
     public:
-        explicit CurseAPIError(std::string  message) : message(std::move(message)) {}
-        [[nodiscard]] const char* what() const noexcept override {
-            return message.c_str();
-        }
+        explicit CurseAPIError(string  message);
+        [[nodiscard]] const char* what() const noexcept override;
 
-        explicit CurseAPIError(const std::exception &e) : message(e.what()) {}
+        explicit CurseAPIError(const exception &e) : message(e.what()) {}
     private:
-        std::string message;
+        string message;
+    };
+
+    class NotFoundError final : public CurseAPIError {
+    public:
+        explicit NotFoundError(string for_path);
     };
 }
+
 
 #endif //CURSEERRORS_HPP
