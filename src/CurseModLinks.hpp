94a330fbff5218225b2dbd617e4ea9e74906c41f
Advik <advik.b@gmail.com>
2024-07-04 20:00:42 +0530

    Create CurseModLinks.hpp

---
+++ src/CurseModLinks.hpp
@@ -0,0 +1,27 @@
+
+#ifndef CURSEMODLINKS_HPP
+#define CURSEMODLINKS_HPP
+
+#include <nlohmann/json.hpp>
+#include <string>
+#include "CurseEngine.hpp"
+
+namespace cf {
+
+using json = nlohmann::json;
+using std::string;
+
+class CurseModLinks {
+    string websiteUrl;
+    string wikiUrl;
+    string issuesUrl;
+    string sourceUrl;
+
+public:
+    static CurseModLinks from_json(const json &data);
+    ~CurseModLinks();
+};
+
+} // cf
+
+#endif //CURSEMODLINKS_HPP
