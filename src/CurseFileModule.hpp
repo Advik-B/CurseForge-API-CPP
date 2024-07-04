a1ae77336ce052cdea5acca8edab82f7f2c659c9
Advik <advik.b@gmail.com>
2024-07-04 21:10:47 +0530

    Create CurseFileModule.hpp

---
+++ src/CurseFileModule.hpp
@@ -0,0 +1,16 @@
+
+#ifndef CURSEFILEMODULE_HPP
+#define CURSEFILEMODULE_HPP
+
+#include <string>
+
+namespace cf {
+using std::string;
+class CurseFileModule {
+    string name;
+    int64_t fingerprint;
+};
+
+} // cf
+
+#endif //CURSEFILEMODULE_HPP
