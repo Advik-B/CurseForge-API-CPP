8d970e44797f6076337c675bc1bb85aec1ec3b3d
Advik <advik.b@gmail.com>
2024-07-04 23:12:03 +0530

    Create CurseErrors.cpp

---
+++ src/CurseErrors.cpp
@@ -0,0 +1,10 @@
+#include "CurseErrors.hpp"
+
+namespace cf::errors {
+    inline const char* CurseAPIError::what() const noexcept {
+        return message.c_str();
+    }
+
+    CurseAPIError::CurseAPIError(string message) : message(move(message)) {}
+    NotFoundError::NotFoundError(string for_path) : CurseAPIError("Not found: " + for_path) {}
+}
