aadf2eb7e2dae618ccbc04264ab5120a11cb0cc7
Advik <advik.b@gmail.com>
2024-07-04 22:50:02 +0530

    Update main.cpp

--- tests/main.cpp
+++ tests/main.cpp
@@ -9,6 +9,7 @@ using namespace std;
 int main() {
     cf::CurseForgeAPI api("$2a$10$evexg5YEn66/ch1BwoJItePBJ.zySowOsp74e3vSFsKNNN7tRtH4u");
     const auto mod = cf::CurseMod::from_id(1, api);
+    cout << mod.name << endl;
     // auto response = api.fetch("/v1/categories");
     // cout << response.dump(4) << endl;
     return 0;
