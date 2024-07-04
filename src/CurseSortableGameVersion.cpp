b12f17047b117a342dcb779d3caaae4670ead3e7
Advik <advik.b@gmail.com>
2024-07-05 00:08:55 +0530

    Update CurseSortableGameVersion.cpp

--- src/CurseSortableGameVersion.cpp
+++ src/CurseSortableGameVersion.cpp
@@ -1,14 +1,17 @@
 
 #include "CurseSortableGameVersion.hpp"
 
+
 namespace cf {
     CurseSortableGameVersion CurseSortableGameVersion::from_json(const json &data) {
         CurseSortableGameVersion version;
+        // std::cout << data.dump(4) << std::endl;
         version.gameVersionName = data["gameVersionName"];
         version.gameVersionPadded = data["gameVersionPadded"];
         version.gameVersion = data["gameVersion"];
         version.gameVersionReleaseDate = data["gameVersionReleaseDate"];
-        if (data.contains("gameVersionTypeId")) {
+
+        if (data.contains("gameVersionTypeId") && !data["gameVersionTypeId"].empty()) {
             version.gameVersionTypeId = data["gameVersionTypeId"];
         }
         return version;
