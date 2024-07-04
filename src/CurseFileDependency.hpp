1dd59e0fcdbb5a7d9f561547bb682eb0bbdd5be5
Advik <advik.b@gmail.com>
2024-07-04 21:28:33 +0530

    Update CurseFileDependency.hpp

--- src/CurseFileDependency.hpp
+++ src/CurseFileDependency.hpp
@@ -3,14 +3,19 @@
 #define CURSEFILEDEPENDENCY_HPP
 
 #include <cstdint>
+#include <nlohmann/json.hpp>
 #include "CurseFileRelationType.hpp"
 
 namespace cf {
+using json = nlohmann::json;
 
 
 class CurseFileDependency {
     int32_t modId;
     CurseFileRelationType relationType;
+
+public:
+    static CurseFileDependency from_json(const json &data);
 };
 
 } // cf
