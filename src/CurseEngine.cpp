3df36fd5095a22c6422b0a26abfb2c98ddead413
Advik <advik.b@gmail.com>
2024-07-04 19:11:04 +0530

    Update CurseEngine.cpp

--- src/CurseEngine.cpp
+++ src/CurseEngine.cpp
@@ -5,13 +5,17 @@ namespace cf {
         this->api_key = api_key;
     }
 
-    json CurseForgeAPI::fetch(const string &url) {
+    json CurseForgeAPI::fetch(string &path) {
+        // If the path starts with a slash, remove it
+        if (url[0] == '/') {
+            url = url.substr(1);
+        }
         auto headers = Header{
                     {"Accept", "application/json"},
                     {"x-api-key", this->api_key},
                     {"Content-Type", "application/json"},
                     };
-        const auto response = Get(Url{CURSEFORGE_API + url}, headers);
+        const auto response = Get(Url{CURSEFORGE_API + path}, headers);
         return json::parse(response.text);
     }
 
