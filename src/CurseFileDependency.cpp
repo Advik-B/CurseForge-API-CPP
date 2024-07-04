109a5f461a3fef801cfad9c45e2fa462a989b843
Advik <advik.b@gmail.com>
2024-07-04 21:28:42 +0530

    Update CurseFileDependency.cpp

--- src/CurseFileDependency.cpp
+++ src/CurseFileDependency.cpp
@@ -2,4 +2,10 @@
 #include "CurseFileDependency.hpp"
 
 namespace cf {
+    CurseFileDependency CurseFileDependency::from_json(const json &data) {
+        CurseFileDependency dependency;
+        dependency.modId = data["modId"];
+        dependency.relationType = static_cast<CurseFileRelationType>(data["relationType"]);
+        return dependency;
+    }
 } // cf
