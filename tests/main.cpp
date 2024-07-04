a34b9b8ef6363149566bfa233d78b642c4731870
Advik <advik.b@gmail.com>
2024-07-04 19:10:57 +0530

    Update main.cpp

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <string>
 #include "CurseEngine.hpp"
+#include "CurseCategory.hpp"
 
 using namespace std;
 
 
 int main() {
     cf::CurseForgeAPI api("$2a$10$evexg5YEn66/ch1BwoJItePBJ.zySowOsp74e3vSFsKNNN7tRtH4u");
-    auto response = api.fetch("v1/games");
+    auto response = api.fetch("/v1/categories");
     cout << response.dump(4) << endl;
     return 0;
 }
