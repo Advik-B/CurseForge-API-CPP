72e85b6d2a035a791132c1a78adcdb939c543fe5
Advik <advik.b@gmail.com>
2024-07-04 21:28:29 +0530

    Update CurseFileHash.cpp

--- src/CurseFileHash.cpp
+++ src/CurseFileHash.cpp
@@ -2,4 +2,10 @@
 #include "CurseFileHash.hpp"
 
 namespace cf {
+    CurseFileHash CurseFileHash::from_json(const json &data) {
+        CurseFileHash hash;
+        hash.value = data["value"];
+        hash.algo = static_cast<CurseHashAlgo>(data["algo"]);
+        return hash;
+    }
 } // cf
