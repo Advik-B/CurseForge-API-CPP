e44f574c378702003a7f8120b95720083b42b0e0
Advik <advik.b@gmail.com>
2024-07-04 22:49:47 +0530

    Update CurseFileModule.hpp

--- src/CurseFileModule.hpp
+++ src/CurseFileModule.hpp
@@ -3,12 +3,18 @@
 #define CURSEFILEMODULE_HPP
 
 #include <string>
+#include <nlohmann/json.hpp>
 
 namespace cf {
 using std::string;
+using json = nlohmann::json;
 class CurseFileModule {
+public:
     string name;
     int64_t fingerprint = 0;
+
+    static CurseFileModule from_json(const json &data);
+
 };
 
 } // cf
