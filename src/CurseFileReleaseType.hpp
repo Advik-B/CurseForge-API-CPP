54ad2f14af7a262a01a26fd2f38d32675965d2dc
Advik <advik.b@gmail.com>
2024-07-04 20:37:24 +0530

    Create CurseFileReleaseType.hpp

---
+++ src/CurseFileReleaseType.hpp
@@ -0,0 +1,15 @@
+
+#ifndef CURSEFILERELEASETYPE_HPP
+#define CURSEFILERELEASETYPE_HPP
+
+namespace cf {
+
+enum class CurseFileReleaseType {
+    RELEASE = 1,
+    BETA = 2,
+    ALPHA = 3
+};
+
+} // cf
+
+#endif //CURSEFILERELEASETYPE_HPP
