d4384cb123012433304fa253af51607353851d2d
Advik <advik.b@gmail.com>
2024-07-04 22:15:45 +0530

    Update CurseModAuthor.cpp

--- src/CurseModAuthor.cpp
+++ src/CurseModAuthor.cpp
@@ -2,4 +2,11 @@
 #include "CurseModAuthor.hpp"
 
 namespace cf {
+    CurseModAuthor CurseModAuthor::from_json(const json &data) {
+        CurseModAuthor author;
+        author.id = data["id"];
+        author.name = data["name"];
+        author.url = data["url"];
+        return author;
+    }
 } // cf
