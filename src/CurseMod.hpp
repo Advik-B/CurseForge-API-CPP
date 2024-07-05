e4e9923ad4cdbac4e8db0517a03a2bb4f637a2fa
Advik <advik.b@gmail.com>
2024-07-05 23:37:02 +0530

    Update CurseMod.hpp

--- src/CurseMod.hpp
+++ src/CurseMod.hpp
@@ -202,7 +202,7 @@ public:
     optional<double> rating = std::nullopt;
 
     static CurseMod from_json(const json &data);
-    const static CurseMod from_id(int32_t id, CurseForgeAPI &engine);
+    static CurseMod from_id(int32_t id, CurseForgeAPI &engine);
 };
 
 }
