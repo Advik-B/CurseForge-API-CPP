d731fdb8785fdd3fa3d9ae9461b7793510d16e6e
Advik <advik.b@gmail.com>
2024-07-13 13:33:53 +0530

    Update CurseSearchBuilder.hpp

--- src/CurseSearchBuilder.hpp
+++ src/CurseSearchBuilder.hpp
@@ -5,22 +5,42 @@
 #include <cstdint>
 #include <string>
 #include <optional>
+#include <nlohmann/json.hpp>
 #include "CurseModsSearchSortField.hpp"
+#include "CurseModLoaderType.hpp"
 
 namespace cf {
 
 using std::int32_t;
 using std::string;
+using std::optional;
+using std::nullopt;
+using json = nlohmann::json;
 
 class CurseSearchBuilder {
 public:
     int32_t gameId;
-    int32_t classId;
-    int32_t categoryId;
-    string categoryIds;
-    string gameVersion;
-    string gameVersions;
-    string searchFilter;
+    optional<int32_t> classId        = nullopt;
+    optional<int32_t> categoryId     = nullopt;
+    optional<string> categoryIds     = nullopt;
+    optional<string> gameVersion     = nullopt;
+    optional<string> gameVersions    = nullopt;
+    optional<string> searchFilter    = nullopt;
+    optional<CurseModsSearchSortField> sortField = nullopt;
+    optional<string> sortOrder = nullopt;
+    optional<CurseModLoaderType> modLoaderType = nullopt;
+    optional<string> modLoaderTypes = nullopt;
+    optional<int32_t> gameVersionTypeId = nullopt;
+    optional<int32_t> authorId = nullopt;
+    optional<int32_t> primaryAuthorId = nullopt;
+    optional<string> slug = nullopt;
+    optional<int32_t> index = nullopt;
+    optional<int32_t> pageSize = nullopt;
+
+    static CurseSearchBuilder Minecraft();
+    static CurseSearchBuilder from_json(const json &data);
+    string to_query_string() const;
+    json to_json() const;
 };
 
 } // cf
