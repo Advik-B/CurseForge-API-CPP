79cfba37d53bdf59d92fc64bddfd9d20d1c48922
Advik <advik.b@gmail.com>
2024-07-13 15:24:33 +0530

    Update main.cpp

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,17 +2,33 @@
 #include <CurseForgeAPI.hpp>
 #include <CurseMod.hpp>
 #include <CurseErrors.hpp>
-// #include "CurseMod.hpp"
+
 using namespace std;
 
-
-int main() {
-    cf::CurseForgeAPI api("$2a$10$evexg5YEn66/ch1BwoJItePBJ.zySowOsp74x3vSFsKNNN7tRtH4u");
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        cout << "Usage: " << argv[0] << " <api-key>" << endl;
+        return 1;
+    }
+    string apiKey = argv[1];
+    cf::CurseForgeAPI api(apiKey);
     try {
-        const auto mod = cf::CurseMod::from_id(238222, api);
+        // auto searchQuery = cf::CurseSearchBuilder::Minecraft();
+        // const auto mods = cf::CurseMod::search(searchQuery, api);
+        // for (const auto &mod : mods) {
+        //     if (mod.isBlocked) {
+        //         cout << "Blocked: ";
+        //         cout << mod.id;
+        //     }
+        // }
+        const auto mod = cf::CurseMod::from_id(374528, api);
+        cout << mod.id << endl;
         cout << mod.name << endl;
         cout << mod.authors[0].name << endl;
+        cout << mod.latestFiles[0].downloadUrl.value_or("nope") << endl;
     } catch (const cf::errors::CurseAPIError& e) {
+        cout << e.what() << endl;
+    } catch (const std::exception& e) {
         cout << e.what() << endl;
     }
     // auto response = api.fetch("/v1/categories");
