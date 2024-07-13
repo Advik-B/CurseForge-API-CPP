8e2e95b4c7e993cf1251fe4f3f1ce592ed96023f
Advik <advik.b@gmail.com>
2024-07-13 14:49:25 +0530

    Update CurseMod.cpp

--- src/CurseMod.cpp
+++ src/CurseMod.cpp
@@ -1,7 +1,22 @@
-
 #include "CurseMod.hpp"
-// #include <iostream>
+#include <string>
+#include <sstream>
+
 namespace cf {
+    std::string guess_download_url(int32_t id, int32_t file_id, std::string &file_name) {
+        const std::string MOD_BASE_URL = "https://edge.forgecdn.net/files/";
+        std::stringstream ss;
+        ss << file_id;
+        std::string file_id_str = ss.str();
+        if (file_id_str[0] == '0') {
+            file_id_str = file_id_str.substr(1);
+        }
+        std::string file_id_1 = file_id_str.substr(0, 4);
+        std::string file_id_2 = file_id_str.substr(4, 3);
+        std::string download_url = MOD_BASE_URL + file_id_1 + "/" + file_id_2 + "/" + file_name;
+        return download_url;
+    }
+
     CurseMod CurseMod::from_json(const json &data) {
         // std::cout << data.dump(4) << std::endl;
         CurseMod mod;
@@ -14,34 +29,39 @@ namespace cf {
         mod.downloadCount = data["downloadCount"];
         mod.isFeatured = data["isFeatured"];
         mod.primaryCategoryId = data["primaryCategoryId"];
-        for (const auto &category : data["categories"]) {
+        for (const auto &category: data["categories"]) {
             mod.categories.push_back(CurseCategory::from_json(category));
         }
         if (data.contains("classId")) {
             mod.classId = data["classId"];
         }
-        for (const auto &author : data["authors"]) {
+        for (const auto &author: data["authors"]) {
             mod.authors.push_back(CurseModAuthor::from_json(author));
         }
         mod.logo = CurseModAsset::from_json(data["logo"]);
-        for (const auto &screenshot : data["screenshots"]) {
+        for (const auto &screenshot: data["screenshots"]) {
             mod.screenshots.push_back(CurseModAsset::from_json(screenshot));
         }
         mod.mainFileId = data["mainFileId"];
-        for (const auto &file : data["latestFiles"]) {
+        for (const auto &file: data["latestFiles"]) {
             mod.latestFiles.push_back(CurseFile::from_json(file));
         }
-        for (const auto &index : data["latestFilesIndexes"]) {
+        for (const auto &index: data["latestFilesIndexes"]) {
             mod.latestFilesIndexes.push_back(CurseFileIndex::from_json(index));
         }
-        for (const auto &index : data["latestEarlyAccessFilesIndexes"]) {
+        for (const auto &index: data["latestEarlyAccessFilesIndexes"]) {
             mod.latestEarlyAccessFilesIndexes.push_back(CurseFileIndex::from_json(index));
         }
         mod.dateCreated = data["dateCreated"];
         mod.dateModified = data["dateModified"];
         mod.dateReleased = data["dateReleased"];
         if (data.contains("allowModDistribution")) {
-            mod.allowModDistribution = data["allowModDistribution"];
+            if (data["allowModDistribution"].is_boolean())
+                mod.allowModDistribution = data["allowModDistribution"];
+            else {
+                mod.allowModDistribution = false;
+                mod.isBlocked = true;
+            }
         }
         mod.gamePopularityRank = data["gamePopularityRank"];
         mod.isAvailable = data["isAvailable"];
@@ -49,6 +69,8 @@ namespace cf {
         if (data.contains("rating")) {
             mod.rating = data["rating"];
         }
+
+
         return mod;
     }
 
@@ -60,9 +82,21 @@ namespace cf {
         vector<CurseMod> mods;
         mods.reserve(50);
         json data = engine.fetch("/v1/mods/search?" + builder.to_query_string())["data"];
-        for (const auto &mod : data) {
+        int index = 0;
+        for (const auto &mod: data) {
+            // std::cout << mod.dump(4) << std::endl;
             mods.push_back(from_json(mod));
+            // std::cout << index++ << std::endl;
         }
         return mods;
+    }
+
+    void CurseMod::unblock() {
+        for (auto &file: latestFiles) {
+            if (!file.downloadUrl.has_value()) {
+                file.downloadUrl = guess_download_url(id, file.id, file.fileName);
+            }
+        }
+        isBlocked = false;
     }
 }
