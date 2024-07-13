ca0b41c36c37463620a147b943e8c99ccf01d221
Advik <advik.b@gmail.com>
2024-07-13 12:58:34 +0530

    Create CurseModsSearchSortField.hpp

---
+++ src/CurseModsSearchSortField.hpp
@@ -0,0 +1,22 @@
+
+#ifndef CURSEMODSSEARCHSORTFIELD_HPP
+#define CURSEMODSSEARCHSORTFIELD_HPP
+
+namespace cf {
+    enum class ModsSearchSortField {
+        FEATURED = 1,
+        POPULARITY = 2,
+        LAST_UPDATED = 3,
+        NAME = 4,
+        AUTHOR = 5,
+        TOTAL_DOWNLOADS = 6,
+        CATEGORY = 7,
+        GAME_VERSION = 8,
+        EARLY_ACCESS = 9,
+        FEATURED_RELEASED = 10,
+        RELEASED_DATE = 11,
+        RATING = 12,
+    };
+}
+
+#endif //CURSEMODSSEARCHSORTFIELD_HPP
